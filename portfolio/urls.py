a496133d904162ce6ab2cf1b3e202b5ec027bc47
Keerthivasan-s-j <jagankeerthisri@gmail.com>
2024-08-07 19:54:57 +0530

    created the signin and signup page

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('in/<str:uname>/', views.user_profile, name="user_profile"),
+    path('login/', views.user_login, name="user_login"),
+    path('signin/', views.user_signin, name="user_signin"),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
+def user_login(request):
+    return render(request, "auth/login.html")
+
+def user_signin(request):
+    return render(request, "auth/signin.html")
+
 def home(request):
     return render(request, "base/home.html")
 
+def user_profile(request, uname):
+    return render(request, "user/profile.html", context={'uname':uname})
 # Create your views here.
