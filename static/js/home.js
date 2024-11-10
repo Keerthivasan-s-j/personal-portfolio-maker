651f44793b0dfe38cfc2401478caf63f18b22107
Keerthivasan-s-j <jagankeerthisri@gmail.com>
2024-11-10 17:22:53 +0530

    added visibility to the edit option in the user profile based on the authentication status

--- static/js/home.js
+++ static/js/home.js
@@ -1,15 +1,16 @@
-document.querySelector(".user-profile-dropdown").onclick = toggleDropDown;
-function toggleDropDown(){
-    var parent = document.querySelector(".user-profile-dropdown");
-    var dropDownMenu = document.querySelector(".dropdown-menu");
-    var dropDownIcon = parent.querySelector("i");
-    if (dropDownMenu.style.display === "" || dropDownMenu.style.display === "none"){
-        dropDownMenu.style.display = "flex";
-        dropDownIcon.classList.replace("fa-chevron-down", "fa-chevron-up");
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".user-profile-dropdown").onclick = toggleDropDown;
+    function toggleDropDown(){
+        var parent = document.querySelector(".user-profile-dropdown");
+        var dropDownMenu = document.querySelector(".dropdown-menu");
+        var dropDownIcon = parent.querySelector("i");
+        if (dropDownMenu.style.display === "" || dropDownMenu.style.display === "none"){
+            dropDownMenu.style.display = "flex";
+            dropDownIcon.classList.replace("fa-chevron-down", "fa-chevron-up");
+        }
+        else{
+            dropDownMenu.style.display = "none";
+            dropDownIcon.classList.replace("fa-chevron-up", "fa-chevron-down");
+        }
     }
-    else{
-        dropDownMenu.style.display = "none";
-        dropDownIcon.classList.replace("fa-chevron-up", "fa-chevron-down");
-    }
-    // dropDownMenu.style.display = dropDownMenu.style.display === "flex" ? "none" : "flex";
-}
+});
