fc385dae297b6cfe22671ceb85fd120b6a3da007
Keerthivasan-s-j <jagankeerthisri@gmail.com>
2024-11-19 21:30:27 +0530

    feat: implemented dynamic data updation in user profile and created models for them

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http.response import HttpResponse
 from django.urls import reverse
+from .models import *
 
 def user_login(request):
     if (request.method == "POST"):
@@ -33,24 +34,62 @@ def user_signin(request):
     return render(request, "auth/signin.html")
 
 def home(request):
+    if request.user.is_authenticated:
+        logged_user = get_object_or_404(UserProfile, user=request.user)
+    else:
+        logged_user = None
+
+    context = {
+        'logged_user': logged_user
+    }
     if(request.method == "POST"):
         search = request.POST.get("search")
         user = User.objects.filter(username = search).first()
         if (user is not None):
             return redirect(reverse("user_profile", kwargs={"uname" : search}))
-    return render(request, "base/home.html")
+    return render(request, "base/home.html", context)
 
 def user_profile(request, uname):
-    if(request.method == "POST"):
-        search = request.POST.get("search")
-        user = User.objects.filter(username = search).first()
-        if (user is not None):
-            return redirect(reverse("user_profile", kwargs={"uname" : search}))
-    user = User.objects.filter(username = uname).first()
-    if (user is None):
-        return HttpResponse("Profile Not Found!")
+    user = get_object_or_404(User, username=uname)
+    user_profile = get_object_or_404(UserProfile, user=user)
+    user_about = user_profile.about_me
+    user_experience = user_profile.experiences.all()
+    user_educations = user_profile.educations.all()
+    user_certificates = user_profile.certificates.all()
+    user_projects = user_profile.projects.all()
+    user_skills = user_profile.skills.all()
+    user_languages = user_profile.languages.all()
+    if request.user.is_authenticated:
+        logged_user = get_object_or_404(UserProfile, user=request.user)
     else:
-        return render(request, "user/profile.html", context={'uname':uname})
+        logged_user = None
+
+    edit_access = uname == request.user.username
+
+    context = {
+        'edit_access': edit_access, 
+        'uname': uname, 
+        'user_profile': user_profile, 
+        'user_about': user_about,
+        'user_experience': user_experience,
+        'user_educations': user_educations,
+        'user_certificates': user_certificates,
+        'user_projects': user_projects,
+        'user_skills': user_skills,
+        'user_languages': user_languages,
+        'logged_user': logged_user,
+        }
+
+    if request.method == "POST":
+        search = request.POST.get("search")
+        searched_user = User.objects.filter(username=search).first()
+
+        if searched_user:
+            return redirect(reverse("user_profile", kwargs={"uname": search}))
+        else:
+            return HttpResponse("Profile Not Found!")
+
+    return render(request, "user/profile.html", context)
 
 def personal_details_form(request):
     return render(request, "forms/personal_details_form.html")
